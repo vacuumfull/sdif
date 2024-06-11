812c0a40993ef1232635e9b7ec0e93f602be1a2b
acdvrtx <acidvortex@MacBook-Pro-acdvrtx.local>
2024-06-11 20:54:10 +0300

    up

--- adapter.py
+++ adapter.py
@@ -2,24 +2,26 @@ import torch
 from diffusers import StableDiffusionPipeline, DDIMScheduler
 from diffusers.utils import load_image
 
-pipeline = StableDiffusionPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5",
-    torch_dtype=torch.float16,
-).to("cuda")
-pipeline.scheduler = DDIMScheduler.from_config(pipeline.scheduler.config)
-pipeline.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter-full-face_sd15.bin")
 
-pipeline.set_ip_adapter_scale(0.5)
-
-image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/ip_adapter_einstein_base.png")
-generator = torch.Generator(device="cpu").manual_seed(26)
-
-image = pipeline(
-    prompt="A photo of Einstein as a chef, wearing an apron, cooking in a French restaurant",
-    ip_adapter_image=image,
-    negative_prompt="lowres, bad anatomy, worst quality, low quality",
-    num_inference_steps=100,
-    generator=generator,
-).images[0]
-print('finished')
-print(image)
+class AdapterClass:
+    def __init__(self):
+        self.pipeline = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5",
+            torch_dtype=torch.float16,
+        ).to("cuda")
+        self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config)
+        self.pipeline.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter-full-face_sd15.bin")
+        self.pipeline.set_ip_adapter_scale(0.5)
+        self.generator = torch.Generator(device="gpu").manual_seed(26)
+        self.image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/ip_adapter_einstein_base.png")
+        
+    
+    def prompt(self, text: str):
+        image = self.pipeline(
+            prompt=text,
+            ip_adapter_image=self.image,
+            negative_prompt="lowres, bad anatomy, worst quality, low quality",
+            num_inference_steps=100,
+            generator=self.generator,
+        ).images[0]
+        return image
--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import sys
 from os import getenv
-
+from bot import AdapterClass
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -12,6 +12,7 @@ from aiogram.utils.markdown import hbold
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_API_KEY")
+PROMPT_COMMAND = "/prompt"
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
@@ -38,6 +39,12 @@ async def echo_handler(message: types.Message) -> None:
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
     try:
+        print(message.text)
+        if PROMPT_COMMAND in message.text:
+            text = message.text.replace(PROMPT_COMMAND, '')
+            adapter = AdapterClass()
+            image = adapter.prompt(text)
+            print(image)
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
