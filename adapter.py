3be916850de2b7b4d973c292f2bc1fddbdf9c16a
acdvrtx <acidvortex@MacBook-Pro-acdvrtx.local>
2024-06-12 13:37:25 +0300

    up

--- adapter.py
+++ adapter.py
@@ -8,6 +8,8 @@ import base64
 
 MODEL_PATH = '../stable-diffusion-webui/models/Stable-diffusion/'
 IMAGE_URL ='./images/'
+NEGATIVE_PROMPT = '(deformed, distorted, disfigured:1.3), poorly drawn,bad anatomy, wrong anatomy, extralimb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation'
+NUM_STEPS = 25
 
 class ImageSd:
     def __init__(self, filename: str = None):
@@ -26,18 +28,27 @@ class ImageSd:
 
 class StableDif:
     def __init__(self, use_adapter = False, uid = None):
-        
+        self.model_name = "Reliberate_v3"
         self.user_id = base64.b64encode(f'{uid}'.encode()).decode() if uid else None
         self.image_sd = ImageSd(self.user_id)
         self.generator = torch.Generator(device="cpu").manual_seed(26)
-        self.model_path = f"{MODEL_PATH}Reliberate_v3.safetensors"
+        self.model_path = f"{MODEL_PATH}{self.model_name}.safetensors"
         self.pipeline = StableDiffusionPipeline.from_single_file(
             self.model_path,
             torch_dtype=torch.float16,
-            use_safetensors=True
+            use_safetensors=True,
+            num_inference_steps=NUM_STEPS,
+            negative_prompt=NEGATIVE_PROMPT,
+            denoising_strength = 0.7
         ).to("cuda")
         self.pipeline_text_to_image = StableDiffusionPipeline.from_single_file(
-            self.model_path, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+            self.model_path, 
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True,
+            num_inference_steps=NUM_STEPS,
+            negative_prompt=NEGATIVE_PROMPT,
+            denoising_strength = 0.7
         ).to("cuda")
         self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config)
         if use_adapter:
@@ -54,40 +65,3 @@ class StableDif:
 
     def delete_image(self, filename):
         return self.image_sd.remove(filename)
-
-
-class AdapterClass:
-    def __init__(self):
-        self.pipeline = StableDiffusionPipeline.from_pretrained(
-            "runwayml/stable-diffusion-v1-5",
-            torch_dtype=torch.float16,
-        ).to("cuda")
-        self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config)
-        self.pipeline.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter-full-face_sd15.bin")
-        self.pipeline.set_ip_adapter_scale(0.5)
-        self.generator = torch.Generator(device="cpu").manual_seed(26)
-        self.pipline_text_to_image = AutoPipelineForText2Image.from_pretrained(
-            "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-        ).to("cuda")
-        self.image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/ip_adapter_einstein_base.png")
-        
-    
-    def prompt_image(self, text: str):
-        image = AutoPipelineForText2Image.from_pretrained(
-            "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-        ).to("cuda")
-        return image
-    
-    def prompt_image(self, text: str):
-        image = self.pipeline_text_to_image(prompt=text).images[0]
-        return image
-
-    @staticmethod
-    def save_image(image):
-        filename = 'test.jpg'
-        image.save(f'./{filename}')
-        return filename
-
-    @staticmethod
-    def remove_image(filename):
-        os.remove(f'./{filename}')
