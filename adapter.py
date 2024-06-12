1be78b5654377b31d79dada0f59c7588a92ce71d
acdvrtx <acidvortex@MacBook-Pro-acdvrtx.local>
2024-06-12 13:11:48 +0300

    up

--- adapter.py
+++ adapter.py
@@ -30,13 +30,13 @@ class StableDif:
         self.user_id = base64.b64encode(f'{uid}'.encode()).decode() if uid else None
         self.image_sd = ImageSd(self.user_id)
         self.generator = torch.Generator(device="cpu").manual_seed(26)
-        self.model_path = f"runwayml/stable-diffusion-v1-5"
-        self.pipeline = StableDiffusionPipeline.from_pretrained(
+        self.model_path = f"{MODEL_PATH}Reliberate_v3.safetensors"
+        self.pipeline = StableDiffusionPipeline.from_single_file(
             self.model_path,
             torch_dtype=torch.float16,
             use_safetensors=True
         ).to("cuda")
-        self.pipeline_text_to_image = AutoPipelineForText2Image.from_pretrained(
+        self.pipeline_text_to_image = AutoPipelineForText2Image.from_single_file(
             self.model_path, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
         ).to("cuda")
         self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config)
